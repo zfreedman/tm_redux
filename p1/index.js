711d062be4b98638ca528703125fc707041679cc
Zach Freedman <zfreedman92@gmail.com>
2018-08-10 13:54:57 -0400

    feat: Added loading reducer to state
    This allows the application to not render the Todos/Goals until they've been fetched from the backend

--- p1/index.js
+++ p1/index.js
@@ -131,8 +131,18 @@ function goals (state = [], action) {
   }
 }
 
+function loading (state = true, action) {
+  switch(action.type) {
+    case RECEIVE_DATA:
+      return false;
+    default:
+      return state;
+  }
+}
+
 // use this stuff below for testing
 const store = Redux.createStore(Redux.combineReducers({
-  todos,
   goals,
+  todos,
+  loading,
 }), Redux.applyMiddleware(checker, logger));
