06acf9025acbb86fdf6ad1e36a2de1b14af6cf23
Zach Freedman <zfreedman92@gmail.com>
2018-08-16 13:06:32 -0400

    feat: Added createStore call to src/index.js
    goals-todos app now mimics p1 functionality

--- goals-todos/src/index.js
+++ goals-todos/src/index.js
@@ -2,5 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import reducers from "./reducers";
+import middleware from "./middleware";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(reducers, middleware);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
