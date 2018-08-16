a5d2b88ac66de0602fe02d915f204fb466a5a8e4
Zach Freedman <zfreedman92@gmail.com>
2018-08-16 12:58:06 -0400

    chore: Ported over components code from p1

--- goals-todos/src/components/App.js
+++ goals-todos/src/components/App.js
@@ -1,13 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { connect } from "react-redux";
+import ConnectedGoals from "./Goals";
+import ConnectedTodos from "./Todos";
+import { handleInitialData } from "../actions/shared";
 
-class App extends Component {
+class App extends React.Component {
   render() {
+    if (this.props.loading === true) {
+      return <h3>Loading</h3>;
+    }
+
     return (
       <div>
-        Hello world
+        <ConnectedTodos />
+        <ConnectedGoals />
       </div>
     );
   }
+
+  componentDidMount () {
+    const { dispatch } = this.props;
+
+    // initial data fetch
+    dispatch(handleInitialData());
+  }
 }
 
-export default App;
+export default connect(state => ({
+  loading: state.loading
+}))(App);
